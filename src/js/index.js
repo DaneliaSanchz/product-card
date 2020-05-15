d7683551d74ebc03b915f52e6f2ec6aea178bad9
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-05-14 21:36:38 -0500

    add background color to the container

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,21 @@ buttonRed.addEventListener('click', changeColor);
 buttonGreen.addEventListener('click', changeColor);
 
 function changeColor(e) {
+  let container = document.querySelector('.imgBx');
   let shoe = document.querySelector('img');
+  let buttonBuy = document.getElementById('btnBuy');
+
   if (e.target.id === 'purple') {
+    container.style.backgroundColor = '#6a2096';
     shoe.style.filter = 'hue-rotate(45deg)';
+    buttonBuy.style.backgroundColor = '#380755';
   } else if (e.target.id === 'red') {
+    container.style.backgroundColor = '#961719';
     shoe.style.filter = 'hue-rotate(120deg)';
+    buttonBuy.style.backgroundColor = '#560203';
   } else {
+    container.style.backgroundColor = '#0d610d';
     shoe.style.filter = 'hue-rotate(230deg)';
+    buttonBuy.style.backgroundColor = '#003000';
   }
 }
